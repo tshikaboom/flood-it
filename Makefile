9809ca16fc181ed6c17cc04cb5c92bccf072a1d9
Oskar Viljasaar <oskar@sandy-bridge.(none)>
2013-03-14 04:35:18 +0100

    Separation du main des strategies/coloriages
    Ca fait des fichiers plus propres!
    Surtout important (...) quand on le rend dans 4h :-)

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
-API= API_Gene_instance.c API_Grille.c Liste_case.c Pile_case.c Zones.c
-FILES= Flood-It-1.c
+SRC=Zones.c API_Grille.c strategie_coloriage.c Pile_case.c API_Gene_instance.c Liste_case.c Flood-It-1.c 
 CFLAGS=-g -Wall
 LDFLAGS=-lSDL
 CC=gcc
 
 all: Flood
 
-Flood: $(FILES:.c=.o) $(API:.c=.o)
+Flood: $(SRC:.c=.o)
 	$(CC) $(CFLAGS)  -o $@ $^ $(LDFLAGS)
 
 %.o: %.c
@@ -19,9 +18,14 @@ clean:
 
 API_Gene_instance.o: API_Gene_instance.c API_Gene_instance.h
 API_Grille.o: API_Grille.c API_Grille.h
+Backup.Flood-It-1.o: Backup.Flood-It-1.c Flood-It-1.h API_Grille.h \
+ API_Gene_instance.h Pile_case.h Liste_case.h Zones.h
+Flood-It-1.o: Flood-It-1.c strategie_coloriage.h Flood-It-1.h \
+ API_Grille.h API_Gene_instance.h Pile_case.h Liste_case.h Zones.h
 Liste_case.o: Liste_case.c Liste_case.h
+main.o: main.c Pile_case.h
 Pile_case.o: Pile_case.c Pile_case.h
+strategie-coloriage.o: strategie-coloriage.c strategie_coloriage.h \
+ Flood-It-1.h API_Grille.h API_Gene_instance.h Pile_case.h Liste_case.h \
+ Zones.h
 Zones.o: Zones.c Liste_case.h Zones.h Pile_case.h
-Flood-It-1.o: Flood-It-1.c Flood-It-1.h API_Grille.h API_Gene_instance.h \
- Pile_case.h Liste_case.h Zones.h
-
---
+++ strategie_coloriage.h
@@ -0,0 +1,49 @@
+#ifndef STRATEGIE_COLORIAGE_H
+#define STRATEGIE_COLORIAGE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Flood-It-1.h"
+
+
+/*
+ * Coloriage de la matrice de maniere imperative
+ * La fonction colorie la case (i, j) et stocke dans une pile la liste des cases de la meme couleur avant de parcourir cette case
+ * Incremente le compteur du nombre de cases changees (taille)
+ * arguments: matrice, colonne, ligne, couleur, pointeur vers taille, taille de la matrice
+ */
+void colorie_zone_imp(int ** tab, int i, int j, int cl, int* taille, int nbCases);
+
+/*
+ * Coloriage de la matrice de maniere recursive
+ * La fonction colorie la case (i,j) et la zone environnante
+ * Effectue un appel recursif si les cases autour sont de la meme couleur que la case visee
+ * Incremente le compteur du nombre de cases changees (taille)
+ * arguments: matrice, colonne, ligne, couleur, pointeur vers taille, taille de la matrice
+ */
+void colorie_zone_rec(int ** tab, int i, int j, int cl, int* taille, int nbCases);
+
+/*
+ *  Strategie aleatoire imperative
+ *  Change aleatoirement la couleur de la premiere case et la zone des cases de sa couleur
+ *  arguments: Matrice, nbCases, nombre de couleurs
+ */
+int strategie_aleatoire_imp(Grille* G, int ** tab, int nbCases, int nbCl);
+
+/*
+ * Strategie aleatoire
+ * Change aleatoirement la couleur de la premiere case et la zone des cases de sa
+ * arguments: Matrice, nbCases, nombre de couleurs
+ */
+int strategie_aleatoire_rec(Grille* G, int ** tab, int nbCases, int nbCl);
+
+/*
+ * Strategie aleatoire avec la methode rapide
+ * Rend le nombre d'iterations
+ */
+int strategie_aleatoire_rapide(Grille* G, int ** tab, int nbCases, int nbCl);
+
+
+
+#endif
